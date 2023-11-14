6558785b9af680fdc7652f1b26629d3f7ffcef35
Magne Tjärnlund <matj1116@storsjogymnasiet.se>
2023-11-14 11:45:46 +0100

    miniräknare

--- lexor/lexor.py
+++ lexor/lexor.py
@@ -1,51 +1,43 @@
 #Jag gjorde 
-def main():
-    gloslista = {}
-    print("-:glosor:-")
-    antal_ratt = 0
-    
-    f = open("demofile2.txt", "w")
-    f.write("gloslista")
-    f.close()
 
-
+gloslista = {}
+print("-:glosor:-")
+antal_ratt = 0
+def Make_glosor():
     while True:
         svglosa = input("\n \tMata in svensk glosa: ")
         englosa = input("\n \tMata in eng glosa: ")
-        
         gloslista.update({svglosa : englosa})
-
-
-
         stoppa = input("\n\t Villl du mata in en glosa till? j/n ")
         if (stoppa == "n"):
             break
+Make_glosor()
 
+def Start_glosförhör():
     while True:
-
+        
         print("\n Nu startar glosförhöret!!!")
         for glosa in gloslista:
-
             svar = input (f"\n{glosa} : ") 
 
-            if svar == gloslista [glosa]:
-                print("Rätt svar")
-                antal_ratt += 1
-                print(f"\n du har {antal_ratt} Rätt!!")
-            else:
-                print(f"\n Fel svar, det är {gloslista[glosa]}")
-                print(f"\n{antal_ratt}")
-        
+        if svar == gloslista [glosa]:
+            print("Rätt svar")
+            antal_ratt += 1
+            print(f"\n du har {antal_ratt} Rätt!!")
+        else:
+            print(f"\n Fel svar, det är {gloslista[glosa]}")
+            print(f"\n{antal_ratt}")
+            
         RAWR = input ("vill du köra om? j/n: ") 
         print(f"\n Du hade {antal_ratt} rätt!!")
         antal_ratt = 0
         if RAWR == "n":
-            f = open("demofile2.txt", "w")
-            f.write()
-            f.close()
-
-            
-            
             break
+Start_glosförhör()
 
-main()
+with open("demofile2.txt", "w") as file:
+    for glosa in gloslista:
+        print(glosa)
+        print(gloslista.get(glosa))
+        file.write(glosa + " " +gloslista.get(glosa))
+        file.close()
---
+++ miniräknare/miniräknare.py
@@ -0,0 +1,27 @@
+print ("vill du andvända plus, minus eller div 1= plus, 2= minus 3= div:")
+choice = input()
+number1 = input("\n \tMata in ett nummer plus: ")
+number2 = input("\n \tMata in ett annat nummer plus: ")
+
+def plus():
+    svar = (int(number1) + int(number2))
+    print ("Svar:")
+    print (svar)
+
+def minus():
+    
+    minus1 = input("\n \tMata in ett nummer minus: ")
+    minus2 = input("\n \tMata in ett annat nummer minus: ")
+
+def div():
+    
+    div1 = input("\n \tMata in ett nummer div: ")
+    div2 = input("\n \tMata in ett annaif choice == 1:")
+                 
+if choice == 1:
+    plus()
+elif choice == 2:
+    minus
+elif choice == 3:
+    div
+
